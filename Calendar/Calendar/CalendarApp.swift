06aaf10d8bce48358c796fcc852450d3190d3b7f
Crane-tsuru <gr26eate@i.softbank.jp>
2024-10-02 17:25:23 +0900

    set up gitignore

---
+++ Calendar/Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by 鶴見駿 on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
