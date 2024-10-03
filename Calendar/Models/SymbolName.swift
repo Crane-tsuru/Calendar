b7bf13288a3eee7abc6f617260f9f63395c27301
Crane-tsuru <gr26eate@i.softbank.jp>
2024-10-04 00:15:52 +0900

    make enum SymbolName for SFSymbol name

---
+++ Calendar/Models/SymbolName.swift
@@ -0,0 +1,11 @@
+//
+//  SymbolName.swift
+//  Calendar
+//
+//  Created by 鶴見駿 on 2024/10/04.
+//
+
+enum SymbolName: String {
+    case calendar = "calendar"
+    case oneday_task = "list.bullet"
+}
---
+++ Calendar/UI/View/Components/LeftSheetButton.swift
@@ -0,0 +1,22 @@
+//
+//  LeftSheetButton.swift
+//  Calendar
+//
+//  Created by 鶴見駿 on 2024/10/04.
+//
+
+import SwiftUI
+
+struct LeftSheetButton: View {
+    let systemName: String
+    
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
+
+#Preview {
+    LeftSheetButton()
+}
