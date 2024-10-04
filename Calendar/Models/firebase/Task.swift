efd8f7d9c3e31e68d676322691a2edf2efb74e7d
Crane-tsuru <gr26eate@i.softbank.jp>
2024-10-04 20:03:14 +0900

    create struct Task

--- Calendar/Models/firebase/Task.swift
+++ Calendar/Models/firebase/Task.swift
@@ -5,4 +5,22 @@
 //  Created by 鶴見駿 on 2024/10/04.
 //
 
-import Foundation
+struct Task {
+    var title: String?
+    var startTime: Int?
+    var endTime: Int?
+    var description: String?
+    var location: String?
+    var color: Int?
+    var isFinished: Bool?
+    
+    init(taskData: [String: Any]) {
+        self.title = taskData["title"] as? String ?? nil
+        self.startTime = taskData["startTime"] as? Int ?? nil
+        self.endTime = taskData["endTime"] as? Int ?? nil
+        self.description = taskData["description"] as? String ?? nil
+        self.location = taskData["location"] as? String ?? nil
+        self.color = taskData["color"] as? Int ?? nil
+        self.isFinished = taskData["isFinished"] as? Bool ?? nil
+    }
+}
--- Calendar/Models/firebase/User.swift
+++ Calendar/Models/firebase/User.swift
@@ -5,4 +5,6 @@
 //  Created by 鶴見駿 on 2024/10/04.
 //
 
-import Foundation
+struct User {
+    var userid: String
+}
