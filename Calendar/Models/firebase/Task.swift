e75f731d2ebbda45cbb47b4999a3be17e6dd0e48
Crane-tsuru <gr26eate@i.softbank.jp>
2024-10-05 00:21:07 +0900

    add propery id for Task and delete somepropery

--- Calendar/Models/firebase/Task.swift
+++ Calendar/Models/firebase/Task.swift
@@ -6,21 +6,16 @@
 //
 
 struct Task {
+    var id: String
     var title: String?
     var startTime: Int?
     var endTime: Int?
     var description: String?
-    var location: String?
-    var color: Int?
-    var isFinished: Bool?
     
     init(taskData: [String: Any]) {
+        self.id = taskData["id"] as? String ?? ""
         self.title = taskData["title"] as? String ?? nil
         self.startTime = taskData["startTime"] as? Int ?? nil
         self.endTime = taskData["endTime"] as? Int ?? nil
-        self.description = taskData["description"] as? String ?? nil
-        self.location = taskData["location"] as? String ?? nil
-        self.color = taskData["color"] as? Int ?? nil
-        self.isFinished = taskData["isFinished"] as? Bool ?? nil
     }
 }
