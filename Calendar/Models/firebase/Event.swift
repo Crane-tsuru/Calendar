8ec41b316ce47f9c0eea10eb304ceb1487adde9b
Crane-tsuru <gr26eate@i.softbank.jp>
2024-10-04 19:23:18 +0900

    make file Task.swift

---
+++ Calendar/Models/firebase/Event.swift
@@ -0,0 +1,9 @@
+//
+//  Event.swift
+//  Calendar
+//
+//  Created by 鶴見駿 on 2024/10/04.
+//
+
+import Foundation
+
---
+++ Calendar/Models/firebase/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Calendar
+//
+//  Created by 鶴見駿 on 2024/10/04.
+//
+
+import Foundation
