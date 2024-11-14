e8d8a254f961c20c705add79b40ae35d25c8fa05
yamamotokento <akatuki@yamamotokentonoMacBook-Pro.local>
2024-11-14 21:06:05 +0900

    create TaskdetailView

---
+++ Calendar/UI/View/TaskdetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskdetailView.swift
+//  Calendar
+//
+//  Created by yamamotokento on 2024/11/07.
+//
+
+import SwiftUI
+
+struct TaskdetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskdetailView()
+}
